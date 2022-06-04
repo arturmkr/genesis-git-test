f787a7a8e123b47a77b2334a2335d00ec4818349
amkrtychian <artur.mkr@gmail.com>
2022-06-04 11:02:48 +0300

    add Hometask

--- weather.py
+++ weather.py
@@ -9,14 +9,18 @@ def get_current_weather_by_city(api_key: str, city: str) -> dict:
     return json.loads(response.text)
 
 
+def kelvin_to_celsius(kelvin: float) -> float:
+    return kelvin - 273.15
+
+
 def print_weather(weather_data: dict):
     print(f"Weather data: {current_city}")
-    print(f"Temp, °C: {round(weather_data['main']['temp'] - 273)}")
-    print(f"Temp feels like, °C: {round(weather_data['main']['feels_like'] - 273)}")
+    print(f"Temp, °C: {round(kelvin_to_celsius(weather_data['main']['temp']))}")
+    print(f"Temp feels like, °C: {round(kelvin_to_celsius(weather_data['main']['feels_like']))}")
     print(f"Humidity, %: {round(weather_data['main']['humidity'])}")
     print(f"Wind speed, m/s: {round(weather_data['wind']['speed'], 1)}")
     print(f"Pressure, hPa: {weather_data['main']['pressure']}")
-    print(f"Visibility, km: {weather_data['visibility'] / 1000}")
+    print(f"Visibility, km: {round(weather_data['visibility'] / 1000, 1)}")
 
 
 if __name__ == "__main__":
